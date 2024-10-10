40f946c11ca809e3d6afda7d06249db127738c99
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2024-10-10 15:24:09 +0100

    ad click counter

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
+const myButton = document.getElementById("my-button");
+const myParagraph = document.getElementById("my-paragraph")
+
+let clickCounter = 0;
 
 
-document.getElementById("my-button").addEventListener("click", clickFunction);
+
+//document.getElementById("my-button").addEventListener("click", clickFunction);
 
 function clickFunction() {
-    document.getElementById("my-paragraph").innerHTML = "WOAW NEW WORDS";
+
+    clickCounter = clickCounter +1;
+
+    myParagraph.innerHTML = "I have been clicked "+ clickCounter + " times";
 }
+
+myButton.onclick = clickFunction;
