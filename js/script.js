81230705a7407126f09a098ef350d2e525230227
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2024-10-10 16:22:21 +0100

    this is NOT working ;;

--- js/script.js
+++ js/script.js
@@ -1,17 +1,50 @@
+//define sources for each character choice
+
 const myButton = document.getElementById("my-button");
-const myParagraph = document.getElementById("my-paragraph")
+const myParagraph = document.getElementById("my-paragraph");
+const characterImage = document.getElementedById("character-img");
 
-let clickCounter = 0;
+// store which character is where in the constant
+const imageOne = "assets/ame-kangel.webp"
+const imageTwo = "assets/kangel.jpeg"
+const imageThree = "assets/glitched-monster.webp"
 
-
+let characterCounter = 0;
 
 //document.getElementById("my-button").addEventListener("click", clickFunction);
 
+/**
+ * 
+ * 
+ * update character counter and display next character
+ * sets counter back to 1 if 3 has been chosen
+ * @returns when character is chosen
+ * ... if it worked
+ */
 function clickFunction() {
 
-    clickCounter = clickCounter +1;
+    characterCounter = characterCounter + 1;
 
-    myParagraph.innerHTML = "I have been clicked "+ clickCounter + " times";
+    if (characterCounter > 3) {
+        characterCounter = 1;
+    }
+    if (characterCounter === 1) {
+        characterImage.src = imageOne;
+        return;
+    }
+    if (characterCounter === 2) {
+        characterImage.src = imageTwo;
+        return;
+    }
+    if (characterCounter === 3) {
+        characterImage.src = imageThree;
+        return;
+    }
+
+
+
+    // characterImage.src = "assets/kangel.jpeg" 
+    myParagraph.innerHTML = "I have been clicked " + characterCounter + " times";
 }
 
 myButton.onclick = clickFunction;
