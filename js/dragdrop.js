03b12235ae17ab9b4235086583fb6dc49258d59b
notAnnabel <Annabel.Dornan@student.norwichuni.ac.uk>
2024-10-15 16:11:42 +0100

    i made comments ;3

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -14,25 +14,36 @@ function onDragOver(event) {
     console.log("Something is being dragged over me!");
 }
 
-
+// offsetx and offsety store the initial offset from clickimg on dragged image
 let offsetX = 0;
 let offsetY = 0;
+
+// 
 let draggedImage = undefined;
 
-
+/**
+ * stores dragimage as the event targer and compute initial offsetx and offsety
+ * @param {Event} event 
+ */
 function onDragStart(event) {
+    // event target points to the html element that start the drag event, ie the currently dragged image
     draggedImage = event.target;
 
+    // 
     const style = window.getComputedStyle(draggedImage, null);
-    
+    // offsetx calculated as clientx horizontal ,mouse pos - style.left (left position of the image)
+    // offsety calculated as clientx vertical ,mouse pos - style.top (top position of the image)
     offsetX = event.clientX - parseInt(style.left);
     offsetY = event.clientY - parseInt(style.top);
     console.log ("I'm being dragged");
 }
 
+
 dropZone.ondrop = onDrop;
 dropZone.ondragover = onDragOver;
 
+
+// look through html elemets stored inside the compliments array
 for (let complement of complements){
     complement.ondragstart = onDragStart;
       complement.ondragover = onDragOver;
